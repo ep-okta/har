3f4eb2f9647fdd43a39637c21091656de342da7f
Emmanuel Paz <emmanuel.paz@okta.com>
2025-06-12 13:27:28 -0600

    testing

--- src/components/har/request-detail-server-ip.tsx
+++ src/components/har/request-detail-server-ip.tsx
@@ -13,6 +13,7 @@ export function RequestDetailServerIP({
     <div className="text-sm">
       <span className="mr-1">🌐</span>
       <span className="text-gray-600 dark:text-gray-400">Server IP:</span>
+      <span className="text-gray-600 dark:text-gray-400">Server IP:</span>
       <span className="text-gray-900 dark:text-gray-100 ml-1">
         {serverIPAddress}
       </span>
